816e056eefedef6425ea3d18a45f9b9bcf03906b
Ayush Raj <ayush.raj138@gmail.com>
2023-06-24 22:55:35 +0530

    fixed: commentbox

--- src/frontend/pages/post/post.jsx
+++ src/frontend/pages/post/post.jsx
@@ -1,13 +1,41 @@
-import { useParams } from "react-router-dom"
-import post from "./post.module.css"
+import { useParams, Link } from "react-router-dom";
+import post from "./post.module.css";
 import { useData } from "../../context/dataContext";
 import { PostCard } from "../../components/postCard/postCard";
 
-export const Post=()=>{
-    const {postId}=useParams();
-    const {postsState}=useData();
-    console.log(postsState)
-    const findPost=postsState.find(({postId:id})=>id===postId)
-    console.log(findPost)
-    return <div className={post.postContainer}><PostCard postData={findPost}/></div>
-}
+export const Post = () => {
+  const { postId } = useParams();
+  const { postsState } = useData();
+  const findPost = postsState.find(({ postId: id }) => id === postId);
+  console.log(findPost);
+  return (
+    <div className={post.postContainer}>
+      <Link to="/">
+        <h2>
+          <span className="material-symbols-outlined">arrow_back</span>
+          Post
+        </h2>
+      </Link>
+      <PostCard postData={findPost} />
+      <div className={post.commentBox}>
+        {findPost?.comments.map((comment, index) => (
+          <div key={index} className={post.comments}>
+            <div>
+              <span className={post.profile}></span>
+            </div>
+            <div>
+              @{comment.username}
+              <p>Replying to @{findPost.username}</p>
+              <p style={{ marginTop: "1rem" }}>{comment.comment}</p>
+              <div className={post.commentFooter}>
+                <span class="material-symbols-outlined">favorite</span>
+                <span class="material-symbols-outlined">chat_bubble</span>
+                <span class="material-symbols-outlined">share</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
